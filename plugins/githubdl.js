e8da99d60515f4d5f293b52433bf9192e365115b
FrazBoyv1 <91239698+FrazBoyv1@users.noreply.github.com>
2021-12-10 16:01:22 +0530

    Update githubdl.js

--- plugins/githubdl.js
+++ plugins/githubdl.js
@@ -1,7 +1,7 @@
 let handler = async (m, { args, usedPrefix, command }) => {
 
-    if (!args[0]) throw 'where is the username bro?\nExample SudoAnirudh'
-    if (!args[1]) throw 'where is the repo??\nExample E-V-A'
+    if (!args[0]) throw 'where is the username bro?\nExample FrazBoyv1'
+    if (!args[1]) throw 'where is the repo??\nExample EVA'
     
     let url = `https://github.com/${args[0]}/${args[1]}/archive/refs/heads/main.zip`
     //ByRizkyAdi
